13edfcf8e7338479dae683419dc100aa7483e107
Mike Miller <mtmiller@octave.org>
2017-04-18 21:15:28 -0700

    Announce build stages, write to separate log files

--- build.sh
+++ build.sh
@@ -2,18 +2,21 @@
 
 set -e
 
-# 1. Build Octave in a virtual machine using Vagrant
+echo "1. Provisioning virtual machine ..."
+vagrant up --provision > guest-provision.log 2>&1
 
-echo "Starting Vagrant and building Octave (may take about 1 hour)..."
-vagrant box update
-vagrant up --provision
-vagrant ssh -c "sh /vagrant/guest-prepare-octave.sh" > vagrant-build.log 2>&1
-vagrant ssh -c "sh /vagrant/guest-build-octave.sh"  >> vagrant-build.log 2>&1
-vagrant ssh -c "sh /vagrant/guest-bundle-octave.sh" >> vagrant-build.log 2>&1
-vagrant ssh -c "mv octave-ubuntu-trusty-snapshot.tar.xz /vagrant" >> vagrant-build.log 2>&1
+echo "2. Preparing user enviroment and pulling Octave source ..."
+vagrant ssh -c "sh /vagrant/guest-prepare-octave.sh" > guest-prepare-octave.log 2>&1
+
+echo "3. Building Octave ..."
+vagrant ssh -c "sh /vagrant/guest-build-octave.sh" > guest-build-octave.log 2>&1
+
+echo "4. Installing and compressing Octave binary image ..."
+vagrant ssh -c "sh /vagrant/guest-bundle-octave.sh" > guest-bundle-octave.log 2>&1
+
+echo "5. Copying Octave binary image ..."
+vagrant ssh -c "cp octave-ubuntu-trusty-snapshot.tar.xz /vagrant" > /dev/null 2>&1
 
 vagrant destroy -f > /dev/null 2>&1
 
-echo
 echo "Now upload the file octave-ubuntu-trusty-snapshot.tar.xz to a public URL"
-echo
