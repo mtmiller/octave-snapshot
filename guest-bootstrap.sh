d2aa518ec0d73071726a835493da6650d489cfb4
Mike Miller <mtmiller@octave.org>
2017-04-21 14:37:26 -0700

    Allow build guest to be configured for Qt 4 or 5

--- guest-bootstrap.sh
+++ guest-bootstrap.sh
@@ -4,8 +4,60 @@
 
 set -xe
 
+n=$(basename "$0")
+t=$(getopt --name "$n" --options "" --longoptions "with-qt:" -- "$@")
+eval set -- "$t"
+
+with_qt=5
+
+while :; do
+  case "$1" in
+    --with-qt)
+      case "$2" in
+        4|5)
+          with_qt=$2
+          ;;
+        *)
+          echo >&2 "$n: invalid option value for --with-qt: $2"
+          exit 1
+          ;;
+      esac
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo >&2 "$n: error handling command line arguments: $1"
+      exit 1
+      ;;
+  esac
+done
+
 apt-get update
 apt-get --yes upgrade
+
+case "$with_qt" in
+  4)
+    qt_packages="
+  libqscintilla2-dev
+  libqt4-dev
+  libqt4-dev-bin
+  libqt4-opengl-dev
+  qt4-dev-tools
+  qt4-linguist-tools
+    "
+    ;;
+  5)
+    qt_packages="
+  libqt5scintilla2-dev
+  qtbase5-dev-tools
+  qttools5-dev
+  qttools5-dev-tools
+    "
+    ;;
+esac
 
 # 1. Install Octave build dependencies
 apt-get --yes build-dep octave
@@ -13,14 +65,12 @@ apt-get --yes install \
   bison \
   git \
   icoutils \
-  libqt5scintilla2-dev \
   librsvg2-bin \
   libsndfile1-dev \
   mercurial \
   portaudio19-dev \
-  qtbase5-dev-tools \
-  qttools5-dev \
-  qttools5-dev-tools
+  qtchooser \
+  $qt_packages
 
 # 2. Install Python build dependencies
 apt-get --yes install \
