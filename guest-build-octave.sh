a315caafe5fdc5735b95b011d96d7b52b3b76536
Mike Miller <mtmiller@octave.org>
2017-04-22 09:05:21 -0700

    Avoid changing working directory where possible

--- guest-build-octave.sh
+++ guest-build-octave.sh
@@ -6,15 +6,14 @@ set -xe
 
 basedir=$HOME
 
-cd $basedir/octave-default
-sh bootstrap
+( cd $basedir/octave-default && sh bootstrap )
 
 mkdir -p $basedir/octave-build
-cd $basedir/octave-build
-sh ../octave-default/configure --build=x86_64-linux \
-                               --with-blas \
-                               --without-osmesa \
-                               JAVA_HOME=/usr/lib/jvm/default-java
-make -j$(getconf _NPROCESSORS_ONLN) all
-diff ../octave-default/HG-ID HG-ID > /dev/null
-cp ../octave-default/HG-TIMESTAMP HG-TIMESTAMP
+( cd $basedir/octave-build \
+  && sh ../octave-default/configure --build=x86_64-linux \
+                                    --with-blas \
+                                    --without-osmesa \
+                                    JAVA_HOME=/usr/lib/jvm/default-java )
+make -C $basedir/octave-build -j$(getconf _NPROCESSORS_ONLN) all
+diff $basedir/octave-default/HG-ID $basedir/octave-build/HG-ID
+cp $basedir/octave-default/HG-TIMESTAMP $basedir/octave-build/HG-TIMESTAMP
