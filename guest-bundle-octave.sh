d5668e23f234daceae73911b63304c365f45da30
Mike Miller <mtmiller@octave.org>
2017-05-23 18:50:35 -0700

    Refactor tar creation options for clarity
    Set tar options in the environment to keep the tar command simple. Add
    `--format=ustar`. Set other environment variables for a reproducible
    environment.

--- guest-bundle-octave.sh
+++ guest-bundle-octave.sh
@@ -13,6 +13,10 @@ case "$version" in
   *+) suffix="$version$hg_id" ;;
   *)  suffix="$version+$hg_id" ;;
 esac
+export SOURCE_DATE_EPOCH=$timestamp
+export TAR_OPTIONS="--format=ustar --owner=root:0 --group=root:0 --sort=name"
+export TAR_OPTIONS="$TAR_OPTIONS --mtime=@$timestamp"
+export TZ=UTC0
 
 filename=octave-ubuntu-trusty-snapshot.tar.xz
 packname=octave-ubuntu-trusty-$suffix
@@ -30,11 +34,4 @@ rm -f $chrootdir/lib/octave/*/lib*.la
 gzip -9n $chrootdir/share/info/*.info* $chrootdir/share/man/man1/*.1
 chmod -R a+rX,u+w,go-w $chrootdir
 
-( cd $basedir \
-  && tar -c \
-         --group=root:0 \
-         --mtime="@$timestamp" \
-         --owner=root:0 \
-         --sort=name \
-         $packname ) \
-  | xz > $basedir/$filename
+( cd $basedir && tar $TAR_OPTIONS -c $packname ) | xz > $basedir/$filename
