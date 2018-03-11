2b51dc9d9207f418f91622b5f90f8fed7745303a
Mike Miller <mtmiller@octave.org>
2018-03-10 16:38:36 -0800

    service: trigger Docker Hub build on success

--- build-service.sh
+++ build-service.sh
@@ -8,9 +8,16 @@ d=$( cd $( dirname $0 ) && pwd )
 
 workdir=$( mktemp -d "${TMPDIR-/tmp}/octave-snapshot.XXXXXX" )
 
+sha256sums_url="https://s3.amazonaws.com/octave-snapshot/public/SHA256SUMS"
+
+success=yes
+
 cd "$workdir"
 
+curl -s "$sha256sums_url" | grep "snapshot.tar" > sha256sums-before
+
 if ! sh $d/build.sh; then
+  success=no
   last_log_file=$( ls -t guest*.log | head -n1 )
   if [ -e "$last_log_file" ]; then
     tail=$( tail -n10 "$last_log_file" )
@@ -35,6 +42,13 @@ Sincerely,
 
 octave-snapshot
 EOF
+fi
+
+if [ x"$success" = xyes ]; then
+  curl -s "$sha256sums_url" | grep "snapshot.tar" > sha256sums-after
+  if ! cmp -s sha256sums-before sha256sums-after; then
+    $d/docker-trigger-build.sh
+  fi
 fi
 
 cd /
